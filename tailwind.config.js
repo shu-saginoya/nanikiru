5da4cd63e10f69be1375499f5893478cff404d16
shu-saginoya <shu.saginoya@gmail.com>
2023-09-13 15:12:23 +0900

    Fix - tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './index.html',
+    './src/*.{vue,js,ts,jsx,tsx}',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/**/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {}
   },
