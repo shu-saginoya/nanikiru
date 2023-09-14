979cbf387d921e22485653fe4950d51662cb5dfe
shu-saginoya <shu.saginoya@gmail.com>
2023-09-14 17:03:36 +0900

    Add - Exception handling component

---
+++ src/components/atoms/ExceptionHandling.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  flag: boolean
+  error: Error | null
+}>()
+</script>
+
+<template>
+  <div v-if="error">読み込みエラー: {{ error.message }}</div>
+  <div v-else-if="flag">
+    <slot />
+  </div>
+  <div v-else>Loading...</div>
+</template>
