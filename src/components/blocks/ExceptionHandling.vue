e76bedbf6a4f84beaf985232ccaed38137f6f7ed
shu-saginoya <shu.saginoya@gmail.com>
2023-09-15 15:16:56 +0900

    Update - ExceptionHeadling component

---
+++ src/components/blocks/ExceptionHandling.vue
@@ -0,0 +1,23 @@
+s
+<script setup lang="ts">
+defineProps<{
+  flag: boolean
+  error: Error | null
+}>()
+</script>
+
+<template>
+  <div v-if="error">
+    <slot name="error">
+      <p>読み込みエラー: {{ error.message }}</p>
+    </slot>
+  </div>
+  <div v-else-if="flag">
+    <slot />
+  </div>
+  <div v-else>
+    <slot name="else">
+      <p>Loading...</p>
+    </slot>
+  </div>
+</template>
