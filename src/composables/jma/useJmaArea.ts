4ed073feb7ef28dfeaefbda78fdd7dbc982c7bc6
shu-saginoya <shu.saginoya@gmail.com>
2023-09-21 11:28:11 +0900

    Update - Define type of useJmaArea

--- src/composables/jma/useJmaArea.ts
+++ src/composables/jma/useJmaArea.ts
@@ -1,6 +1,8 @@
 import { ref, watch, readonly } from 'vue'
 import { useFetch } from '@/composables/utils/useFetch'
 
+import type { Areas } from '@/types/regional'
+
 export const useJmaArea = () => {
   const url = 'https://www.jma.go.jp/bosai/common/const/area.json'
   const { data, error } = useFetch(url)
@@ -12,11 +14,12 @@ export const useJmaArea = () => {
   const class20s = ref()
   watch(data, () => {
     if (data.value) {
-      centers.value = data.value.centers
-      offices.value = data.value.offices
-      class10s.value = data.value.class10s
-      class15s.value = data.value.class15s
-      class20s.value = data.value.class20s
+      const areas: Areas = data.value
+      centers.value = areas.centers
+      offices.value = areas.offices
+      class10s.value = areas.class10s
+      class15s.value = areas.class15s
+      class20s.value = areas.class20s
     }
   })
 
--- src/types/regional.ts
+++ src/types/regional.ts
@@ -1,3 +1,19 @@
 export type Regional = string | undefined
 export type RegionalList = [string, string, string] | undefined
 export type Level = 1 | 2 | 3
+
+export type Area = {
+  name: string
+  enName: string
+  kana?: string
+  officeName?: string
+  parent?: string
+  children?: string[]
+}
+export type Areas = {
+  centers: Area[]
+  offices: Area[]
+  class10s: Area[]
+  class15s: Area[]
+  class20s: Area[]
+}
