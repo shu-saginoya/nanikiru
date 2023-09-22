2a0e0ffa8f54d83b7a28efe97716f8bef2f3b0e4
shu-saginoya <shu.saginoya@gmail.com>
2023-09-22 17:07:57 +0900

    Update - StateDisplay component

--- src/components/organisms/StateDisplay.vue
+++ src/components/organisms/StateDisplay.vue
@@ -4,23 +4,14 @@ import { storeToRefs } from 'pinia'
 import { useJmaArea } from '@/composables/jma/useJmaArea'
 import { useRegionalNumbersStore } from '@/store/regionalNumbers'
 
-import ExceptionHandling from '@/components/blocks/ExceptionHandling.vue'
-
-const { centers, offices, class10s, error } = useJmaArea()
-const { regionalLv1, regionalLv2, regionalLv3 } = storeToRefs(useRegionalNumbersStore())
-const { setRegionalLv1, setRegionalLv2, setRegionalLv3, initRegional, saveRegional } =
-  useRegionalNumbersStore()
-
-initRegional()
-const select1 = ref()
-const select2 = ref()
-const select3 = ref()
+const { offices, error } = useJmaArea()
+const { regionalLv2 } = storeToRefs(useRegionalNumbersStore())
 </script>
 
 <template>
   <section>
     <p>千葉県流山市</p>
-    <button type="button">地域を変更する</button>
+    <router-link to="select-regional">地域を変更する</router-link>
     <p>9月22日金曜日</p>
   </section>
 </template>
