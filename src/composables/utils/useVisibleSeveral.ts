ab0a6927a6bde2b45618cf6cd21bce96edee6cf7
shu-saginoya <shu.saginoya@gmail.com>
2023-09-27 14:47:42 +0900

    Update - useVisibleSeveral

--- src/composables/utils/useVisibleSeveral.ts
+++ src/composables/utils/useVisibleSeveral.ts
@@ -2,7 +2,7 @@ import { ref, readonly } from 'vue'
 
 export const useVisibleSeveral = (maxValue?: number, loop?: boolean) => {
   const visible = ref<number>(0)
-  const up = (): void => {
+  const next = (): void => {
     if (maxValue && visible.value >= maxValue - 1) {
       if (loop) {
         visible.value = 0
@@ -14,7 +14,7 @@ export const useVisibleSeveral = (maxValue?: number, loop?: boolean) => {
     }
   }
 
-  const down = (): void => {
+  const prev = (): void => {
     if (visible.value > 0) {
       visible.value--
     } else {
@@ -28,7 +28,7 @@ export const useVisibleSeveral = (maxValue?: number, loop?: boolean) => {
 
   return {
     visible: readonly(visible),
-    up,
-    down
+    next,
+    prev
   }
 }
