9f575ecc5a9716ac7974eb0515248dcf0a01e248
shu-saginoya <shu.saginoya@gmail.com>
2023-10-03 13:26:40 +0900

    Update - About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import router from '@/router'
 import ArticleCard from '@/components/molecules/ArticleCard.vue'
+import LinkRouter from '@/components/atoms/LinkRouter.vue'
+import IconBack from '@/components/icons/IconBack.vue'
 </script>
 
 <template>
@@ -13,6 +16,7 @@ import ArticleCard from '@/components/molecules/ArticleCard.vue'
       <p>
         このアプリはそんな忙しい親御さんのために、一目で気温と服装の目安がわかるように開発されました。
       </p>
+      <p>もちろん、おとなの方のお洋服選びにもご利用ください。</p>
       <p>ぜひ、スマホのディスクトップにブックマークして役立ててください。</p>
     </section>
     <section>
@@ -25,8 +29,13 @@ import ArticleCard from '@/components/molecules/ArticleCard.vue'
       </p>
       <p>
         また、アプリ内のアイコンについてはTailwind
-        Toolboxからお借りしました。https://www.tailwindtoolbox.com/
+        Toolboxから利用しています。https://www.tailwindtoolbox.com/icons
       </p>
+    </section>
+    <section class="text-center">
+      <LinkRouter to="/" class="rounded-sm">
+        <div class="flex"><IconBack />もどる</div>
+      </LinkRouter>
     </section>
   </ArticleCard>
 </template>
