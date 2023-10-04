357f2a18c3c6904025d4da3ffc5a9533d7987f76
shu-saginoya <shu.saginoya@gmail.com>
2023-10-04 17:00:54 +0900

    Add Google Analytics #1

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.use(
+  VueGtag,
+  {
+    config: { id: 'G-SL0PEX1BVX' }
+  },
+  router
+)
 
 app.mount('#app')
