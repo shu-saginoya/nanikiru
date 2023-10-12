3d1f591ed2f9c150004c3307aa48da484f94bd2f
shu-saginoya <shu.saginoya@gmail.com>
2023-10-12 16:40:57 +0900

    Add - SpanChip component

--- src/components/atoms/SpanChip.vue
+++ src/components/atoms/SpanChip.vue
@@ -7,10 +7,18 @@ const props = defineProps<{
 
 const colorStyle = computed(() => {
   switch (props.color) {
-    case 'rose':
-      return 'bg-orange-300'
     case 'pink':
+      return 'bg-pink-300'
+    case 'amber':
       return 'bg-amber-300'
+    case 'orange':
+      return 'bg-orange-300'
+    case 'purple':
+      return 'bg-purple-300'
+    case 'slate':
+      return 'bg-slate-300'
+    case 'slate-dark':
+      return 'bg-slate-500'
     default:
       return 'bg-gray-300'
   }
@@ -19,7 +27,7 @@ const colorStyle = computed(() => {
 
 <template>
   <span
-    class="inline-block text-white px-1 align-text-bottom rounded-2xl text-sm tracking-tight"
+    class="inline-block text-white px-1.5 align-text-bottom rounded-2xl text-sm tracking-tight"
     :class="colorStyle"
   >
     <slot />
