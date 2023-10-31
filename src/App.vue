6ca0d6c8f8f02bcef1e426484c110f294598ad44
shu-saginoya <shu.saginoya@gmail.com>
2023-10-31 14:23:53 +0900

    Update - App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import CommonWrapper from '@/components/templates/CommonWrapper.vue'
-import CommonHeader from '@/components/templates/CommonHeader.vue'
-import CommonMain from '@/components/templates/CommonMain.vue'
+import CommonWrapper from '@/components/layouts/CommonWrapper.vue'
+import CommonHeader from '@/components/layouts/CommonHeader.vue'
+import CommonMain from '@/components/layouts/CommonMain.vue'
 </script>
 
 <template>
